b50fc859a1ae047352e63fc8276221c736c0d013
Ali Sanad <alisanad099@gmail.com>
2022-07-08 00:22:20 +0200

    add typescript

--- src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <h1 style={{ textAlign: 'center', backgroundColor: 'lightbue' }}>Hello World REACT!</h1>;
-}
-
-export default App;
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 style={{ textAlign: "center", backgroundColor: "lightblue" }}>
+      Hello React.js with Parcel !
+    </h1>
+  );
+}
+
+export default App;
